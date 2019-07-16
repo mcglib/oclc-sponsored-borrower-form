59fddaac8db1dd220153e040a4b1cb55cc96b975
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2019-07-16 15:18:25 -0400

    Updated the step1 form

--- resources/views/borrower/create-step1.blade.php
+++ resources/views/borrower/create-step1.blade.php
@@ -67,38 +67,34 @@
                     {!! Form::input('text', 'borrower_lname', $borrower->borrower_lname ?? null, ['class'=> 'form-control', 'required' => 'required']) !!}
                 </div>
                 <div class="form-group">
-                    <label for="borrower_email" class="control-label required">Borrower's Email Address <span class="required">*</span></label>
+                    <label for="borrower_email" class="control-label required">Email address <span class="required">*</span></label>
                     {!! Form::input('email', 'borrower_email', $borrower->borrower_email ?? null, ['class'=> 'form-control', 'required' => 'required']) !!}
-                </div>
-                <div class="form-group">
-                    <label for="borrower_address" class="control-label">Mailing Address</label>
-                    {!! Form::textarea('borrower_address',$borrower->borrower_address ?? null, array('class'=>'form-control','rows' => 8, 'cols' => 50)) !!}
                 </div>
 
                 <div class="form-group">
                     <label for="address1" class="control-label">Street address 1<span class="required">*</span></label>
-                 {!! Form::input('text', 'address1', $borrower->address1 ?? null, ['class'=> 'form-control']) !!}
+                     {!! Form::input('text', 'address1', $borrower->address1 ?? null, ['class'=> 'form-control']) !!}
                 </div>
                <div class="form-group">
-                <label for="address2" class="control-label">Street address 2</label>
-                 {!! Form::input('text', 'address2', $borrower->address2 ?? null, ['class'=> 'form-control']) !!}
-
+                    <label for="address2" class="control-label">Street address 2</label>
+                     {!! Form::input('text', 'address2', $borrower->address2 ?? null, ['class'=> 'form-control']) !!}
                </div>
 
                 <div class="form-group">
-                <label for="city" class="control-label">City<span class="required">*</span></label>
-                 {!! Form::input('text', 'city', $borrower->city ?? null, ['class'=> 'form-control']) !!}
+                    <label for="city" class="control-label">City<span class="required">*</span></label>
+                     {!! Form::input('text', 'city', $borrower->city ?? null, ['class'=> 'form-control']) !!}
                 </div>
                 <div class="form-group">
-                <label for="province" class="control-label">Province/State<span class="required">*</span></label>
-                 {!! Form::input('text', 'province_state', $borrower->province_state ?? null, ['class'=> 'form-control']) !!}
+                    <label for="province" class="control-label">Province/State<span class="required">*</span></label>
+                     {!! Form::input('text', 'province_state', $borrower->province_state ?? null, ['class'=> 'form-control']) !!}
                 </div>
                 <div class="form-group">
-                <label for="postal_code" class="control-label">Postal code<span class="required">*</span></label>
-                 {!! Form::input('text', 'postal_code', $borrower->postal_code ?? null, ['class'=> 'form-control', 'placeholder' => 'XXX XXX']) !!}
+                    <label for="postal_code" class="control-label">Postal code<span class="required">*</span></label>
+                     {!! Form::input('text', 'postal_code', $borrower->postal_code ?? null, ['class'=> 'form-control', 'placeholder' => 'XXX XXX']) !!}
                 </div>
-
-                hr />
+                <br />
+                <hr />
+                <br />
                <div class="form-group">
                 <label for="borrower_auth_from" class="control-label">Period of Authorization From<span class="required">*</span></label>
                 {!! Form::date('borrower_auth_from',$borrower->borrower_auth_from ?? \Carbon\Carbon::now()->format('Y-m-d'), ['class' => 'form-control', 'required' => 'required']) !!}
@@ -106,17 +102,6 @@
                <div class="form-group">
                 <label for="borrower_auth_to" class="control-label">Period of Authorization To<span class="required">*</span></label>
                 {!! Form::date('borrower_auth_to', $borrower->borrower_auth_to ?? \Carbon\Carbon::today()->addYears(1)->format('Y-m-d'), ['class' => 'form-control', 'required' => 'required']) !!}
-               </div>
-               <div class="form-group">
-               <label for="borrower_status" class="control-label">Borrower's status<span class="required">*</span></label><br />
-               {{ Form::radio('borrower_status', $borrower->borrower_status ?? null, true ) }} The authorized borrower is a McGill student.
-               {{ Form::radio('borrower_status', $borrower->borrower_status ?? null, false) }} The authorized borrower is not a McGill student.
-               </div>
-                <hr />
-                <p>Please fill in the phone number and address if the borrower is Not a McGill student.</p>
-               <div class="form-group">
-                    <label for="borrower_telephone" class="control-label ">Telephone Number </label>
-                    {!! Form::input('text', 'borrower_telephone', $borrower->borrower_telephone ?? null, ['class'=> 'form-control']) !!}
                </div>
             </fieldset>
             <hr />
