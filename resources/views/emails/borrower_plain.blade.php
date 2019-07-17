a6093f0422f713d2b83db622128440b16f669f9d
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2019-07-17 15:52:09 -0400

    Updated the plain text email for borrower

--- resources/views/emails/borrower_plain.blade.php
+++ resources/views/emails/borrower_plain.blade.php
@@ -1,4 +1,4 @@
-Dear {{ $borrower->fname}} {{ $borrower->lname}},
+Dear {{ $borrower->borrower_fname}} {{ $borrower->borrower_lname}},
 
 This is a confirmation that a library account has been created for you.Please bring this temporary barcode to a Library Services desk: 
 {{$borrower->barcode }}
