f7f98bee055285bb49ff0f2c83378db6aeebe820
Mutugi Gathuri <mutugi.gathuri@gmail.com>
2019-07-17 15:44:40 -0400

    Updated the helloword message

---
+++ resources/views/helloworld.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="col-sm-offset-2 col-sm-8">
+            <div class="alert alert-success" role="alert">{{ $message }}</div>
+        </div>
+    </div>
+@endsection
