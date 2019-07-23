13994ef9be7f38b8d2f8838d0078a5f500f69ee5
Git Library <dev.library@qoml.library.mcgill.ca>
2019-07-23 09:53:53 -0400

    Updated the account details view

--- resources/views/emails/account_details.blade.php
+++ resources/views/emails/account_details.blade.php
@@ -1,11 +1,9 @@
-
-
 <table class="table">
-	<tr>
-				<td>Branch Library:</td>
-				<td><strong>{{$borrower->branch_library_name}}</strong></td>
-				</tr>
-    <tr>
+   <tr>
+		<td>Branch Library:</td>
+		<td><strong>{{$borrower->branch_library_name}}</strong></td>
+   </tr>
+   <tr>
 	<td>First name:</td>
 	<td><strong>{{$borrower->borrower_fname}}</strong></td>
     </tr>
@@ -25,29 +23,26 @@
 	<td>Sponsored Borrower category:</td>
 	<td><strong>{{$borrower->getBorrowerCategoryLabel($borrower->borrower_cat)}}</strong>
 	</td>
-	</tr>
-	<tr>
+     </tr>
+     <tr>
 			<td>Authorized from:</td>
-			<td><strong>{{$borrower->borrower_startdate}}</strong>
-			</td>
-			</tr>
-    <tr>
-			<tr>
-					<td>Authorized from:</td>
-					<td><strong>{{$borrower->borrower_startdate}}</strong>
-					</td>
-					</tr>
-			<tr>
-	<td>Address:</td>
-	<td><strong>
-		<address>
-		{{$borrower->borrower_address1}}
-		{{$borrower->borrower_address2}}<br />
-		{{$borrower->borrower_city}}<br />
-		{{$borrower->borrower_province_state}}<br />
-		{{$borrower->borrower_postal_code}}<br/>
-		</address>
-	    </strong>
-	</td>
+			<td><strong>{{$borrower->borrower_startdate}}</strong></td>
     </tr>
+    <tr>
+			<td>Authorized to:</td>
+			<td><strong>{{$borrower->borrower_enddate}}</strong></td>
+    </tr>
+    <tr>
+		<td>Address:</td>
+		<td><strong>
+			<address>
+			{{$borrower->borrower_address1}}
+			{{$borrower->borrower_address2}}<br />
+			{{$borrower->borrower_city}}<br />
+			{{$borrower->borrower_province_state}}<br />
+			{{$borrower->borrower_postal_code}}<br/>
+			</address>
+		    </strong>
+		</td>
+     </tr>
 </table>
